651f5ce600b7e8c35b8da3d750aeb4de443e2395
Bamvor Zhang <bamv2005@gmail.com>
2018-10-02 19:36:42 +0800

    script: add example for fix width converting
    Signed-off-by: Bamvor Zhang <bamv2005@gmail.com>

--- public/images/games/convert.sh
+++ public/images/games/convert.sh
@@ -1,4 +1,6 @@
 # make image to 25%:
-# for f in `ls Dragon*.png`; do convert -resize 25% $f ${f/.*}__small.png; done
+#   for f in `ls Dragon*.png`; do convert -resize 25% $f ${f/.*}__small.png; done
+# for fix width 800:
+#   convert -resize 800 C5B5576B-EDF9-4DC6-B6EB-92081A1D7024.jpeg # C5B5576B-EDF9-4DC6-B6EB-92081A1D7024__small.jpeg
 file=`cat Dragon_Quest_VI__fighting_process__file_list`
 convert -delay 100 -loop 0 $file animation.gif
